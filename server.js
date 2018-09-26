2b908b86620b3266a37cb3fffe41efefadb6f01c
Jayhua <jordanhua97@gmail.com>
2018-09-25 20:12:58 -0700

    heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/nyt", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/nyt", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
